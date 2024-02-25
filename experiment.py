22e55667419871bb89bf8de0b0a01592451bf3f8
DanielVered <sbhtkurs@gmail.com>
2024-02-25 11:51:47 +0200

    experiment refactor

--- experiment.py
+++ experiment.py
@@ -1,6 +1,8 @@
 import random
+from typing import Dict, List, Any
+
 import pandas as pd
-from avl_template_new import AVLTree
+from avl_template_new import AVLTree, AVLNode
 
 
 # -------------------------------- Experimental Analysis -------------------------------- #
@@ -14,16 +16,16 @@ def split(node) -> []:
 
     parent = node.get_parent()
     while parent is not None:
+        n_joins += 1  # delete me!!
         if node.is_left_son():
             right_subtree = AVLTree.tree_from_root(parent.get_right())
             join_cost = bigger_tree.join(right_subtree, parent.get_key(), parent.get_value())
-            n_joins += 1  # delete me!!
         else:  # node is right son
             left_subtree = AVLTree.tree_from_root(parent.get_left())
             join_cost = left_subtree.join(smaller_tree, parent.get_key(), parent.get_value())
-            n_joins += 1  # delete me!!
-            smaller_tree = left_subtree
+            smaller_tree.replace_tree(left_subtree)
 
+        print(join_cost)
         total_join_cost += join_cost
         if join_cost > max_join_cost:
             max_join_cost = join_cost
@@ -53,31 +55,28 @@ def generate_rand_trees(exp: int) -> (AVLTree, AVLTree):
     return tree1, tree2
 
 
-def random_split(tree: AVLTree, res: dict) -> [AVLTree, AVLTree]:
-    key = random.randint(1, tree.size + 1)
-    node = tree.node_search(key)
+def split_and_record(tree: AVLTree, node: AVLNode, res: dict, split_type: str):
     res["split_key"].append(node.key)
     res["depth"].append(tree.root.height - node.height)
     split_trees, n_joins, max_join_cost, total_join_cost = split(node)
-    res["split_type"].append("random")
+    res["split_type"].append(split_type)
     res["n_joins"].append(n_joins)
     res["max_join_cost"].append(max_join_cost)
     res["total_join_cost"].append(total_join_cost)
+
+
+def random_split(tree: AVLTree, res: dict) -> [AVLTree, AVLTree]:
+    key = random.randint(1, tree.size + 1)
+    node = tree.node_search(key)
+    split_and_record(tree, node, res, split_type="random")
 
 
 def max_split(tree: AVLTree, res: dict) -> [AVLTree, AVLTree]:
     node = tree.predecessor(tree.root)
-
-    res["split_key"].append(node.key)
-    res["depth"].append(tree.root.height - node.height)
-    split_trees, n_joins, max_join_cost, total_join_cost = split(node)
-    res["split_type"].append("max")
-    res["n_joins"].append(n_joins)
-    res["max_join_cost"].append(max_join_cost)
-    res["total_join_cost"].append(total_join_cost)
+    split_and_record(tree, node, res, split_type="max")
 
 
-def experiment() -> pd.DataFrame:
+def experiment() -> dict[str, list[Any]]:
     res = {
         "exponent": []
         , "split_type": []
@@ -88,7 +87,7 @@ def experiment() -> pd.DataFrame:
         , "depth": []
     }
 
-    for exp in range(1, 11):
+    for exp in range(1, 7):
         print(f"Conducting experiment for n={1000 * 2 ** exp} ...")
         tree1, tree2 = generate_rand_trees(exp)
         res["exponent"].extend([exp, exp])
@@ -96,6 +95,10 @@ def experiment() -> pd.DataFrame:
         random_split(tree1, res)
         max_split(tree2, res)
 
+    return res
+
+
+def parse_results(res: dict) -> pd.DataFrame:
     df = pd.DataFrame(res)
     df["mean_cost_per_join"] = df["total_join_cost"] / df["n_joins"]
     df.drop(columns=["total_join_cost", "n_joins"], inplace=True)
@@ -104,13 +107,12 @@ def experiment() -> pd.DataFrame:
     max_ = df[df["split_type"] == 'max']
 
     df = rand.merge(max_, how='inner', on='exponent', suffixes=('_rand', '_max'))
-    df.drop(columns=['split_type_rand', 'split_type_max'], inplace=True)
+    df.drop(columns=['split_type_rand', 'split_type_max', 'split_key_rand', 'split_key_rand'], inplace=True)
     df = df.round(2)
     return df
 
 
-df = experiment()
-df = df[['exponent', 'mean_cost_per_join_rand', 'max_join_cost_rand', 'mean_cost_per_join_max', 'max_join_cost_max']]
+df = parse_results(experiment())
 df.to_excel('exp_res.xlsx')
 
 pass
